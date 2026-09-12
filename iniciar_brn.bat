09c3607b8ace294f9d606032f0284f52d6789c20
Bruno Labanca de Oliveira <brunolabncaoliveira@gmail.com>
2026-09-12 00:17:58 -0300

    Atualizar o iniciar_brn.bat

--- iniciar_brn.bat
+++ iniciar_brn.bat
@@ -3,7 +3,7 @@ setlocal
 cd /d "%~dp0"
 
 REM ==== Token publico do Ngrok ====
-set "NGROK_AUTHTOKEN=COLE_AQUI_SEU_TOKEN_PUBLICO"
+set "NGROK_AUTHTOKEN=3J8xHeVX46aXrOZeXnKrVVFMLTr"
 
 REM ==== Sobe o no BRN em uma janela separada ====
 start "BRN Node 6001" cmd /k python bruno_blockchain_real.py 6001
